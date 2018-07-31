5a0d9531aa4667ff4fb508a828ef0a49d6a91fd3
Sara Magdits <saramagdits@gmail.com>
2018-07-31 12:49:00 -0400

    Added mixins for mobile-first breakpoints

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ const gulp = require('gulp'),
     cssImport = require('postcss-import'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
-    autoprefixer = require('autoprefixer');
+    autoprefixer = require('autoprefixer'),
+    mixins = require('postcss-mixins');
 
 gulp.task('styles', function () {
     // we include return because gulp.src is an asynchronous function
     return gulp.src('./assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
         .on('error', function(errorInfo){
             console.log(errorInfo.toString());
             this.emit('end');
